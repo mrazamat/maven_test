66ee9405e6ce5f142c5ede30b0b272796ab26a0b
Azamat <azamat00189@mail.ru>
2018-12-23 14:12:51 +0500

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Watch watch = new Watch();
+        System.out.println("Current time: " + watch.currentTime);
+    }
+}
---
+++ src/main/java/Watch.java
@@ -0,0 +1,5 @@
+import org.joda.time.LocalTime;
+
+public class Watch {
+    LocalTime currentTime = new LocalTime();
+}
